15048483fa672a2d2ea5d2342040f0211bfe5827
paing_soe_thaw <paingsoethaw@gmail.com>
2018-04-10 01:26:32 +0800

    code clean

--- server.js
+++ server.js
@@ -4,13 +4,12 @@ const app = express()
 const MongoClient = require('mongodb').MongoClient
 var ObjectId = require('mongodb').ObjectID;
 
-// app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
 var db
 MongoClient.connect('mongodb://lab_user:lab_user123@ds239309.mlab.com:39309/one_day_test', (err, client) => {
     if (err) return console.log(err)
-    db = client.db('one_day_test') // whatever your database name is
+    db = client.db('one_day_test')
     app.listen(3000, () => {
         console.log('listening on 3000')
     })
@@ -26,40 +25,40 @@ app.get('/', (req, res) => {
     })
 })
 
+
 //   challenge 1: POST request, save product information
 app.post('/api/product', (req, res) => {
-    // console.log(req.body)
+    var result_json = {
+        api_status: "",
+        data: {}
+    };
     db.collection('product').save(req.body, (err, result) => {
-        var result_json = {
-            api_status: "",
-            data: {}
-        };
         if (err) {
             result_json.api_status = "error";
+            result_json.data = err;
             res.status(500).json(result_json);
         } else {
             result_json.api_status = "good";
             result_json.data = result.ops[0];
-            res.status(200).json(result_json);
+            res.status(201).json(result_json);
         }
     })
 })
 
 //   challenge 2: GET request, get product information by ID
 app.get('/api/product', (req, res) => {
-    //   console.log(req.query);
-    db.collection('product').findOne({ "_id": new ObjectId(req.query.id) }, function (err, result) {
-        // console.log(result);
-        var result_json = {
-            api_status: "",
-            data: {}
-        };
+    var result_json = {
+        api_status: "",
+        data: {}
+    };
+    db.collection('product').findOne({ "_id": new ObjectId(req.query.id) }, function (err, created_result) {
         if (err) {
             result_json.api_status = "error";
+            result_json.data = err;
             res.status(500).json(result_json);
         } else {
             result_json.api_status = "good";
-            result_json.data = result;
+            result_json.data = created_result;
             res.status(200).json(result_json);
         }
     })
@@ -67,51 +66,41 @@ app.get('/api/product', (req, res) => {
 
 //   challenge 3: PUT request, update product information by ID
 app.put('/api/product/:id', function (req, res) {
-    // console.log(req.body);
-    // console.log(req.params.id);
-
+    var result_json = {
+        api_status: "",
+        data: {}
+    };
     db.collection('product').findOneAndUpdate(
         { "_id": new ObjectId(req.params.id) },
-        {
-            $set: req.body
-        },
+        { $set: req.body },
         { new: true, returnOriginal: false }
-    ).then(function (coll) {
-        console.log(coll);
-        res.json({
-            error: false,
-            data: coll.value
-        })
+    ).then(function (updated_result) {
+        result_json.api_status = "good";
+        result_json.data = updated_result.value;
+        res.status(200).json(result_json);
+    }).catch(function (err) {
+        result_json.api_status = "error";
+        result_json.data = err;
+        res.status(500).json(result_json)
     })
-        .catch(function (err) {
-            res.status(500).json({
-                error: true,
-                data: {
-                    message: err.message
-                }
-            })
-        })
 })
 
 //   challenge 4: Delete request, delete product information by ID
 app.delete('/api/product/:id', function (req, res) {
+    var result_json = {
+        api_status: "",
+        data: []
+    };
     db.collection('product').deleteOne(
         { "_id": new ObjectId(req.params.id) },
         { new: true, returnOriginal: false }
-    )
-        .then(function (coll) {
-
-            res.json({
-                error: false,
-                data: [req.params.id]
-            })
-        })
-        .catch(function (err) {
-            res.status(500).json({
-                error: true,
-                data: {
-                    message: err.message
-                }
-            })
-        })
+    ).then(function (deleted_result) {
+        result_json.api_status = "good";
+        result_json.data.push(req.params.id);
+        res.status(200).json(result_json);
+    }).catch(function (err) {
+        result_json.api_status = "error";
+        result_json.data = err;
+        res.status(500).json(result_json)
+    })
 })
